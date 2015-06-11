c2da51fa3b137d69c605537b6fbdcf5fa7d315f0
Riley McGavick <rmcgavick@gmail.com>
2015-06-11 13:56:21 -0700

    minor changes to GUI/JFrame and JLabel sizes

--- src/MapGenerator/GUI.java
+++ src/MapGenerator/GUI.java
@@ -9,42 +9,37 @@ public class GUI extends JFrame{
 	
 	// Set myMapSize to set the desired width of the map, the aspect ratio
 	// should 
-	private final static int myMapSize = 18; 
-	private final static double aspectRatio = (16.0/10.0);
+	private final static int myMapWidth = 26;
+	private final static int myMapHeight = 16;
 	private final static int tilePixelWidth = 60;
 	private final static int tilePixelHeight = 60;
 	
 	private static void createGUI() {
-		int x = (int)((myMapSize * aspectRatio) * tilePixelHeight) + 6;
-		int y = (myMapSize * tilePixelWidth) + 6;
-		System.out.println("Aspect ratio = " + aspectRatio);
-		System.out.println("X and Y values for this map: " + x + ", " + y);
+		
+		int y = (myMapHeight * tilePixelHeight) + 28;
+		int x = (myMapWidth * tilePixelWidth) + 6;
+
 		JFrame mapFrame = new JFrame("Riley's Map Generator");
 		mapFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		mapFrame.setPreferredSize(new Dimension(x, y));
+		mapFrame.setLayout(new FlowLayout(FlowLayout.LEFT, 0, 0));
 		
-		MapGen mg = new MapGen(myMapSize);
-		mg.setLayout(new FlowLayout(FlowLayout.LEADING, 0, 0));
-		mg.setSize(x, y);
+		MapGen mg = new MapGen(myMapHeight, myMapWidth);
+		mg.setLayout(new FlowLayout(FlowLayout.LEFT, 0, 0));
+		mg.setPreferredSize(new Dimension(x, y));
 		mapFrame.getContentPane().add(mg);
-		System.out.println(mg.getSize());
-		mapFrame.setSize(x, y);
-		System.out.println(mapFrame.getSize());
 		
 		mapFrame.pack();
 		mapFrame.setVisible(true);
-		//mapFrame.setResizable(false);
+		mapFrame.setResizable(false);
 	}
 	
-	// simple randomize method, just uses random number generator
-
 	public static void main(String[] args) {
 		javax.swing.SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
 				createGUI();
 			}
 		});
-
 	}
 
 }
--- src/MapGenerator/MapGen.java
+++ src/MapGenerator/MapGen.java
@@ -7,20 +7,19 @@ import javax.swing.*;
 
 public class MapGen extends JPanel {
 
-	protected int x; // map width
-	protected int y; // map height
+	protected int x; // height of map in number of tiles
+	protected int y; // width of map in number of tiles
 	protected MapTile[][] map;
 	protected JLabel[][] mapTiles;
 	
-	// this method takes one int - the desired map width, and the
-	// height is calculated using a 16:10 aspect ratio
-	public MapGen(int ms) {
-		x = ms;
-		y = (int)(16.0/10.0) * ms;
-		map = new MapTile[x][y];
+	public MapGen(int mapWidth, int mapHeight) {
+		y = mapHeight;
+		x = mapWidth;
+		map = new MapTile[y][x];
 		mapTiles = randomizeMap(x, y);
 	}
 	
+	// simple randomize method, just uses random number generator
 	protected JLabel[][] randomizeMap(int width, int height) {
 		String c = "";
 		JLabel[][] mt = new JLabel[height][width];
@@ -29,7 +28,6 @@ public class MapGen extends JPanel {
 			for(int j = 0; j < width; j++) {
 				Random rn = new Random();
 				int n = rn.nextInt((5 - 0) + 1) + 0;
-				//System.out.println("n = " + n);
 				switch(n) {
 					case 0: c = "DeepWater"; 
 								break;
@@ -44,7 +42,6 @@ public class MapGen extends JPanel {
 					case 5: c = "Mountains"; 
 								break;
 				}
-				//System.out.println("c = " + c);
 				map[i][j] = new MapTile(i, j, c);
 				mt[i][j] = new JLabel(new ImageIcon(map[i][j].getTileImage()));
 				add(mt[i][j]);
